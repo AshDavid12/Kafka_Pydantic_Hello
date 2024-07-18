938c925ca5a56965010d8f1b137d74d796b33361
AshDavid12 <gaashdavid@gmail.com>
2024-07-18 16:01:49 +0300

    in functions

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ from pydantic import BaseModel
 class MessageModel(BaseModel):
     text: str
 
-async def main():
-    # Kafka configuration
-    kafka_bootstrap_servers = 'localhost:9092'
-    
+kafka_bootstrap_servers = 'localhost:9092'
+message = MessageModel(text='Hello!')
+        
+# Serialize the Pydantic model to JSON
+message_json = message.json()
+async def producer():
     # Initialize the producer
     producer = AIOKafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
     await producer.start()
+    try:
+        await producer.send_and_wait('test-topic', value=message_json.encode('utf-8'))
+    finally:
+        await producer.stop()
 
-    # Initialize the consumer
+
+async def consumer():
     consumer = AIOKafkaConsumer(
         'test-topic',
         bootstrap_servers=kafka_bootstrap_servers,
@@ -21,30 +28,24 @@ async def main():
         auto_offset_reset='earliest'
     )
     await consumer.start()
-
     try:
-        # Create an instance of the Pydantic model
-        message = MessageModel(text='Hello world!')
-        
-        # Serialize the Pydantic model to JSON
-        message_json = message.json()
-        print(f'Sending message: {message_json}')
-        
-        # Produce a message to the "hello-world" topic
-        await producer.send_and_wait('test-topic', value=message_json.encode('utf-8'))
-        print("Message sent successfully!")
-        
-        # Consume the message
-        async for msg in consumer: #async loop 
+        async for msg in consumer:
             message_json = msg.value.decode('utf-8') #byes format get back to json format
             message = MessageModel.parse_raw(message_json) #get it back to pydantic object
             print(f"Consumed message: {message.text}") #text arrtibute of pydantic object
             break  # Exit after consuming one message
-
     finally:
-        # Stop the producer and consumer
-        await producer.stop()
         await consumer.stop()
 
-# Run the event loop
+async def main():
+    await producer()
+    await consumer()
 asyncio.run(main())
+  
+
+
+
+
+        
+ 
+
